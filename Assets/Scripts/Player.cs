290c35afd62178ba2c57db3a27ad242beab0b3e9
johnmorris4 <johnmorris4@bellsouth.net>
2021-02-22 04:53:55 -0500

    Finished setting up the Movement of the game Cube

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed = 3.5f;
+
+    //public float horizontalInput;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +19,27 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * 5 * Time.deltaTime);
+        CalculateMovement();
+        
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+                float verticalInput = Input.GetAxis("Vertical");
+        
+                Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+                
+                transform.Translate(direction * _speed * Time.deltaTime);
+
+
+                transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -4.5f, 0), 0);
+                if (transform.position.x >= 9.24f)
+                {
+                    transform.position = new Vector3(-9.24f, transform.position.y, 0);
+                } else if (transform.position.x <= -9.24f)
+                {
+                    transform.position = new Vector3(9.24f, transform.position.y, 0);
+                }
     }
 }
